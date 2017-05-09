e5270a7aa3a2c715fe0c10470e6dd55cb2567635
kkomaweb83 <kkomaweb@naver.com>
2017-05-09 11:39:46 +0900

    web.xml 제거

--- kkomaweb01/src/com/danacom/control/pro/ProController.java
+++ kkomaweb01/src/com/danacom/control/pro/ProController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +13,7 @@ import com.danacom.model.base.BaseCommand;
 import com.danacom.model.pcl.PclTopListCommand;
 import com.danacom.model.pro.ProTopSearchCommand;
 
+@WebServlet("/ProController")
 public class ProController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
